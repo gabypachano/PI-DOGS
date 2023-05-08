0d7cf99532b6e641857b0b7c852f7e5870774279
gabypachano <gabrielapachano.13@gmail.com>
2023-05-08 13:40:15 -0300

    avances front

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,26 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 //     Imagen. // Nombre. // Temperamentos. // Peso.
-const Card = ({image, name, temperament, weightMin, weightMax}) => {
+const Card = ({id, image, name, temperament, weightMin, weightMax}) => {
     return(
         <div>
             <h3>{name}</h3>
-            <img src={image} alt="img not found" width="400px" height="450px"/>
-            <h3>{weightMin}</h3>
-            <h3>{weightMax}</h3>
+            <Link to={`/detail/${id}`}  >
             <div>
-                {
-                    console.log(temperament)
-                    // temperament?.map((temp, index) => {
-                    //     return(
-                    //         <div>
-                    //         key={index}
-                    //         {temp}
-                    //         </div>
-                    //     )
-                    // })
-                }
+            <img src={image} alt="img not found" width="400px" height="450px"/>
             </div>
+            </Link>
+            <h3>Weight: {weightMin} - {weightMax} Kg</h3>
+            <h3>Temperament: {temperament}</h3>
         </div>
     )
 }
--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -4,15 +4,14 @@ import { Link } from "react-router-dom";
 
 const Cards = ({dogs}) => {
     const [currentPage, setCurrentPage] = useState(1)
-    const [dogsPerPage, setDogsPerPage] = useState(8) // Mi paginado es de 8 perros por pagina
+    const [dogsPerPage, /* setDogsPerPage */] = useState(8) // Mi paginado es de 8 perros por pagina
     const indexOfLastDog = currentPage * dogsPerPage // 1 * 8 = 8
     const indexOfFirstDog = indexOfLastDog - dogsPerPage // 8 - 8 = 0
     const currentDogs = dogs.slice(indexOfFirstDog, indexOfLastDog) // Me traigo mi estado global, este estado va a tener todos los perritos
  
     
     const pageNumber = [];
-
-    for (let i=0; i <=Math.ceil(dogs/dogsPerPage); i++) {
+    for (let i=0; i <= Math.ceil(dogs/dogsPerPage); i++) {
         pageNumber.push(i)
     }
 
@@ -20,12 +19,14 @@ const Cards = ({dogs}) => {
         setCurrentPage(pageNumber)
     }
 
+
     return(
+        <>
         <div>
             {
-                currentDogs?.map(dog => (
+                currentDogs?.map((dog, index) => (
                     <Card 
-                    key={dog.id}
+                    key={index}
                     id={dog.id}
                     name={dog.name}
                     image={dog.image}
@@ -40,6 +41,17 @@ const Cards = ({dogs}) => {
                 ))
             }
         </div>
+
+        <div>
+            {
+                pageNumber?.map((number, index) => {
+                    return(
+                        <button key={index} onClick={() => paginado(number)}>{number}</button>
+                    )
+                })
+            }
+        </div>
+        </>
     )
 
 
